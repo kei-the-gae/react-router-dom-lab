39c5905605d9ac4f359db7bab60992c0d5af264a
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-19 15:53:31 -0700

    Add new mailbox form

--- src/components/MailboxForm.jsx
+++ src/components/MailboxForm.jsx
@@ -1,5 +1,36 @@
-const MailboxForm = () => {
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+const MailboxForm = () => {
+    const [formData, setFormData] = useState({
+        _id: 0,
+        boxSize: '',
+        boxholder: '',
+    });
+    const navigate = useNavigate();
+
+    return (
+        <main>
+            <h2>New Mailbox</h2>
+            <form>
+                <label htmlFor="boxSize">Box size: </label>
+                <select name="boxSize" id="boxSize">
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
+                </select>
+
+                <label htmlFor="boxholder">Boxholder: </label>
+                <input type="text" name="boxholder" id="boxholder" />
+            </form>
+        </main>
+    );
 };
 
 export default MailboxForm;
+
+// const mailboxExample = {
+//     _id: 1,
+//     boxSize: 'Small',
+//     boxholder: 'Alex',
+//   };

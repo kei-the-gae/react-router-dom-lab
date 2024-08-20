90fd22161280e1c5bf5cc3175f9224733001c24a
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-19 18:50:41 -0700

    Add mail-box class to mailbox list

--- src/components/MailboxList.jsx
+++ src/components/MailboxList.jsx
@@ -6,7 +6,7 @@ const MailboxList = ({ mailboxes }) => {
             <h1>Mailbox List</h1>
             <ul>
                 {mailboxes.map(mailbox => (
-                    <li key={mailbox._id}><Link to={`/mailboxes/${mailbox._id}`}>Mailbox {mailbox._id}</Link></li>
+                    <li key={mailbox._id} className='mail-box'><Link to={`/mailboxes/${mailbox._id}`}>Mailbox {mailbox._id}</Link></li>
                 ))}
             </ul>
         </main>
